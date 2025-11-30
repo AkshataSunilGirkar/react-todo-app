b92eeae269f99e01b94ec69b7636a86c85713b89
Akshata.Girkar <akshata.girkar@ril.com>
2025-11-30 13:33:32 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,193 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [displayTodo, setDisplayTodos] = useState([]);
+  const [error, setError] = useState("");
+  const [toDelete, setToDelete] = useState(null);
+  const [toEdit, setToEdit] = useState(null);
+  const [editInput, setEditInput] = useState(null);
+  const [updateError, setUpdateError] = useState("");
+  const [draggableItem, setDraggableItem] = useState("");
+  const [dragOver, setDragOver] = useState(null);
+
+  function setInputTodo() {
+    setError("");
+  }
+
+  const onTodoFormSubmit = (e) => {
+    e.preventDefault();
+    const formdata = new FormData(e.target);
+    const inputValue = formdata.get("todoInput");
+    if (inputValue.length > 0) {
+      setTodos((prev) => [...prev, { id: new Date().getTime(), name: inputValue }]);
+      e.target.reset();
+    } else {
+      setError("Todo cannot be empty!!");
+    }
+  };
+
+  function onDeleteTodoClick(id) {
+    setToDelete(id);
+    setError("");
+    setToEdit(null);
+    setEditInput("");
+  }
+
+  const handleDeleteConfirm = () => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== toDelete));
+    setToDelete(null);
+  };
+
+  const handleDeleteCancel = () => {
+    setToDelete(null);
+  };
+
+  function onEditTodoClick(id) {
+    setToEdit(id);
+    setEditInput(todos.filter((todo) => todo.id === id)?.[0]?.name);
+    setError("");
+    setToDelete(null);
+  }
+
+  function handleEditInputTodo(e) {
+    setEditInput(e.target.value);
+    setUpdateError("");
+  }
+
+  function handleUpdateTodo() {
+    if (editInput?.length > 0) {
+      setTodos((prev) =>
+        prev.map((todo) =>
+          todo.id === toEdit
+            ? {
+                ...todo,
+                name: editInput,
+              }
+            : todo
+        )
+      );
+      setEditInput("");
+      setToEdit(null);
+    } else {
+      setUpdateError("Todo cannot be empty!!");
+    }
+  }
+
+  function handleUpdateCancel() {
+    setEditInput("");
+    setToEdit(null);
+    setUpdateError("");
+  }
+
+  function handleSearch(e) {
+    setDisplayTodos(() => todos.filter((todo) => todo.name.includes(e.target.value)));
+  }
+
+  useEffect(() => {
+    setDisplayTodos(todos);
+  }, [todos]);
+
+  function handleDragStart(e, item) {
+    setDraggableItem(item);
+    e.dataTransfer.setData("text/plain", "");
+  }
+  function handleDragEnd() {
+    setDraggableItem(null);
+    setDragOver(null);
+  }
+  function handleDragOver(e, todoId) {
+    e.preventDefault();
+    setDragOver(todoId);
+  }
+  function handleDrop(e, todo) {
+    const draggableIndex = todos.indexOf(draggableItem);
+    const ondropIndex = todos.indexOf(todo);
+    if (draggableIndex > -1 && ondropIndex > -1) {
+      setTodos((prevTodos) => {
+        const newTodo = [...prevTodos];
+        newTodo.splice(draggableIndex, 1);
+        newTodo.splice(ondropIndex, 0, draggableItem);
+        return newTodo;
+      });
+    }
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={onTodoFormSubmit}>
+        <div className="inputContainer">
+          <input type="text" id="todoInput" placeholder="Enter todo" name="todoInput" onChange={setInputTodo} />
+          <button type="submit">Add</button>
+        </div>
+        <div className="error">{error}</div>
+      </form>
+      {todos.length > 1 && (
+        <div className="searchContainer">
+          <input type="text" placeholder="Search todo" name="todoSearch" onChange={handleSearch} />
+        </div>
+      )}
+      <div className="todos">
+        {todos && todos.length > 0 ? (
+          displayTodo.map((todo) => {
+            return (
+              <div
+                className={`todoItem ${draggableItem?.id === todo.id ? "draggable" : ""} ${dragOver === todo.id ? "dragover" : ""}`}
+                key={todo.id}
+                draggable
+                onDragStart={(e) => handleDragStart(e, todo)}
+                onDragEnd={handleDragEnd}
+                onDragOver={(e) => handleDragOver(e, todo.id)}
+                onDrop={(e) => handleDrop(e, todo)}
+              >
+                <div className="todo">{todo.name}</div>
+                <button className="deleteTodo" onClick={() => onDeleteTodoClick(todo.id)}>
+                  Delete
+                </button>
+                <button className="editTodo" onClick={() => onEditTodoClick(todo.id)}>
+                  Edit
+                </button>
+                <div>Drag</div>
+              </div>
+            );
+          })
+        ) : (
+          <div>No todos added yet</div>
+        )}
+      </div>
+      {toDelete && (
+        <div className="deleteForm">
+          <div className="formTitle">Are you sure to delete?</div>
+          <div>Todo : {todos.filter((todo) => todo.id === toDelete)?.[0]?.name}</div>
+          <div className="btnContainer">
+            <button className="delete" onClick={handleDeleteConfirm}>
+              Delete
+            </button>
+            <button className="cancel" onClick={handleDeleteCancel}>
+              Cancel
+            </button>
+          </div>
+        </div>
+      )}
+
+      {toEdit && (
+        <div className="editForm">
+          <div className="formTitle">Update Todo</div>
+          <input type="text" placeholder="Enter todo" name="todoEdit" onChange={handleEditInputTodo} value={editInput} />
+          <div className="error">{updateError}</div>
+          <div className="btnContainer">
+            <button className="update" onClick={handleUpdateTodo}>
+              Update
+            </button>
+            <button className="cancel" onClick={handleUpdateCancel}>
+              Cancel
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/svg.jsx
@@ -0,0 +1,103 @@
+export const editSvg = (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <path
+        d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z"
+        stroke="#000000"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13"
+        stroke="#000000"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </g>
+  </svg>
+);
+
+export const addSvg = (
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#y">
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <title></title>
+      <g id="Complete">
+        <g id="add-square">
+          <g>
+            <rect
+              data-name="--Rectangle"
+              fill="none"
+              height="20"
+              id="_--Rectangle"
+              rx="2"
+              ry="2"
+              stroke="#000000"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              width="20"
+              x="2"
+              y="2"
+            ></rect>
+            <line fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="15.5" x2="8.5" y1="12" y2="12"></line>
+            <line fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="12" x2="12" y1="15.5" y2="8.5"></line>
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+
+export const deleteSvg = (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M3 6H21M5 6V20C5 21.1046 5.89543 22 7 22H17C18.1046 22 19 21.1046 19 20V6M8 6V4C8 2.89543 8.89543 2 10 2H14C15.1046 2 16 2.89543 16 4V6"
+      stroke="#000000"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path d="M14 11V17" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+    <path d="M10 11V17" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+  </svg>
+);
+
+export const cancelSvg = (
+  <svg
+    viewBox="0 0 24 24"
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-labelledby="cancelIconTitle"
+    stroke="#000000"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    fill="none"
+    color="#000000"
+  >
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <title id="cancelIconTitle">Cancel</title>
+      <path d="M15.5355339 15.5355339L8.46446609 8.46446609M15.5355339 8.46446609L8.46446609 15.5355339"></path>
+      <path d="M4.92893219,19.0710678 C1.02368927,15.1658249 1.02368927,8.83417511 4.92893219,4.92893219 C8.83417511,1.02368927 15.1658249,1.02368927 19.0710678,4.92893219 C22.9763107,8.83417511 22.9763107,15.1658249 19.0710678,19.0710678 C15.1658249,22.9763107 8.83417511,22.9763107 4.92893219,19.0710678 Z"></path>
+    </g>
+  </svg>
+);
+
+export const saveSvg = (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <path d="M7.29417 12.9577L10.5048 16.1681L17.6729 9" stroke="#000000" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path>
+      <circle cx="12" cy="12" r="10" stroke="#000000" strokeWidth="2"></circle>
+    </g>
+  </svg>
+);
